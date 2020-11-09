b6e982b8fd708ea0f65e83316c03ebcb4f821cb5
BorisBritva1204 <72190041+BorisBritva1204@users.noreply.github.com>
2020-11-09 12:59:40 +0300

    Add files via upload

--- sql/unit1.pas
+++ sql/unit1.pas
@@ -18,12 +18,18 @@ type
     Button3: TButton;
     DataSource1: TDataSource;
     DBGrid1: TDBGrid;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
     MySQL57Connection1: TMySQL57Connection;
     SQLQuery1: TSQLQuery;
     SQLTransaction1: TSQLTransaction;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
+    procedure Edit1Change(Sender: TObject);
+    procedure Edit2Change(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
 
@@ -33,7 +39,7 @@ type
 
 var
   Form1: TForm1;
-
+ a1,a2,a3: integer;
 implementation
 
 {$R *.lfm}
@@ -41,6 +47,11 @@ implementation
 { TForm1 }
 procedure TForm1.Button1Click(Sender: TObject);
 begin
+a1 := 1;
+a2 := 0;
+a3 := 0;
+label1.Caption:= 'Название книги';
+label2.Caption:= 'Автор';
 SQLQuery1.Close;
 SQLQuery1.SQL.Clear;
 SQLQuery1.SQL.Add('Select title as "Название", author as "Автор", published as "Дата издания", stock as "Количество" from books;');
@@ -48,7 +59,12 @@ SQLQuery1.Open;
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
-begin                                          //members borrowings
+begin
+a1 := 0;
+a2 := 1;
+a3 := 0;
+label1.Caption:= 'Фамилия';
+label2.Caption:= 'Имя';
 SQLQuery1.Close;
 SQLQuery1.SQL.Clear;
 SQLQuery1.SQL.Add('Select firstname as "Имя", lastname as "Фамилия" from members;');
@@ -57,15 +73,130 @@ end;
 
 procedure TForm1.Button3Click(Sender: TObject);
 begin
+a1 := 0;
+a2 := 0;
+a3 := 1;
+label1.Caption:= 'Фамилия';
+label2.Caption:= 'Имя';
 SQLQuery1.Close;
 SQLQuery1.SQL.Clear;
-SQLQuery1.SQL.Add('SELECT members.firstname AS "", members.lastname AS "", books.title AS "", borrowings.borrowdate AS "", borrowings.returndate AS "", FROM borrowings JOIN books ON borrowings.bookid=books.bookid JOIN members ON members.memberid=borrowings.memberid');
+SQLQuery1.SQL.Add('SELECT members.firstname AS "Имя", members.lastname AS "Фамилия", books.title AS "Книга", borrowings.borrowdate AS "Когда взял", borrowings.returndate AS "Когда отдать" FROM borrowings JOIN books ON borrowings.bookid=books.bookid JOIN members ON borrowings.memberid=members.memberid');
 SQLQuery1.Open;
+end;
+
+procedure TForm1.Edit1Change(Sender: TObject);
+begin
+if a1=1 then
+  begin
+  SQLQuery1.Close;
+  SQLQuery1.SQL.Clear;
+  if ((sender as TEdit).Name = 'Edit1') then
+     begin
+       SQLQuery1.SQL.Add('Select title as "Название", author as "Автор", published as "Дата издания", stock as "Количество"  from books WHERE author LIKE :authSearch;');
+       SQLQuery1.params.parambyname('authSearch').asstring := edit1.Text + '%';
+     end;
+    if ((sender as TEdit).Name = 'Edit2') then
+     begin
+       SQLQuery1.SQL.Add('Select title as "Название", author as "Автор", published as "Дата издания", stock as "Количество"  from books WHERE title LIKE :titleSearch;');
+       SQLQuery1.params.parambyname('titleSearch').asstring := edit2.Text+'%';
+     end;
+  SQLQuery1.Open;
+end;
+
+if a2=1 then
+  begin
+  SQLQuery1.Close;
+  SQLQuery1.SQL.Clear;
+  if ((sender as TEdit).Name = 'Edit1') then
+     begin
+       SQLQuery1.SQL.Add('Select firstname as "Имя", lastname as "Фамилия" from members WHERE firstname LIKE :fn;');
+       SQLQuery1.params.parambyname('fn').asstring := edit1.Text + '%';
+     end;
+    if ((sender as TEdit).Name = 'Edit2') then
+     begin
+       SQLQuery1.SQL.Add('Select firstname as "Имя", lastname as "Фамилия" from members WHERE lastname LIKE :ln;');
+       SQLQuery1.params.parambyname('ln').asstring := edit2.Text+'%';
+     end;
+  SQLQuery1.Open;
+end;
+
+if a3=1 then
+  begin
+  SQLQuery1.Close;
+  SQLQuery1.SQL.Clear;
+  if ((sender as TEdit).Name = 'Edit1') then
+     begin
+       SQLQuery1.SQL.Add('SELECT members.firstname AS "Имя", members.lastname AS "Фамилия", books.title AS "Книга", borrowings.borrowdate AS "Когда взял", borrowings.returndate AS "Когда отдать" FROM borrowings JOIN books ON borrowings.bookid=books.bookid JOIN members ON borrowings.memberid=members.memberid WHERE firstname LIKE :fn;');
+       SQLQuery1.params.parambyname('fn').asstring := edit1.Text + '%';
+     end;
+    if ((sender as TEdit).Name = 'Edit2') then
+     begin
+       SQLQuery1.SQL.Add('SELECT members.firstname AS "Имя", members.lastname AS "Фамилия", books.title AS "Книга", borrowings.borrowdate AS "Когда взял", borrowings.returndate AS "Когда отдать" FROM borrowings JOIN books ON borrowings.bookid=books.bookid JOIN members ON borrowings.memberid=members.memberid WHERE lastname LIKE :ln;');
+       SQLQuery1.params.parambyname('ln').asstring := edit2.Text+'%';
+     end;
+  SQLQuery1.Open;
+end;
+end;
+
+
+procedure TForm1.Edit2Change(Sender: TObject);
+begin
+if a1=1 then
+  begin
+  SQLQuery1.Close;
+  SQLQuery1.SQL.Clear;
+  if ((sender as TEdit).Name = 'Edit1') then
+     begin
+       SQLQuery1.SQL.Add('Select title as "Название", author as "Автор", published as "Дата издания", stock as "Количество"  from books WHERE author LIKE :authSearch;');
+       SQLQuery1.params.parambyname('authSearch').asstring := edit1.Text + '%';
+     end;
+    if ((sender as TEdit).Name = 'Edit2') then
+     begin
+       SQLQuery1.SQL.Add('Select title as "Название", author as "Автор", published as "Дата издания", stock as "Количество"  from books WHERE title LIKE :titleSearch;');
+       SQLQuery1.params.parambyname('titleSearch').asstring := edit2.Text+'%';
+     end;
+  SQLQuery1.Open;
+end;
+
+if a2=1 then
+  begin
+  SQLQuery1.Close;
+  SQLQuery1.SQL.Clear;
+  if ((sender as TEdit).Name = 'Edit1') then
+     begin
+       SQLQuery1.SQL.Add('Select firstname as "Имя", lastname as "Фамилия" from members WHERE firstname LIKE :fn;');
+       SQLQuery1.params.parambyname('fn').asstring := edit1.Text + '%';
+     end;
+    if ((sender as TEdit).Name = 'Edit2') then
+     begin
+       SQLQuery1.SQL.Add('Select firstname as "Имя", lastname as "Фамилия" from members WHERE lastname LIKE :ln;');
+       SQLQuery1.params.parambyname('ln').asstring := edit2.Text+'%';
+     end;
+  SQLQuery1.Open;
+end;
+
+if a3=1 then
+  begin
+  SQLQuery1.Close;
+  SQLQuery1.SQL.Clear;
+  if ((sender as TEdit).Name = 'Edit1') then
+     begin
+       SQLQuery1.SQL.Add('SELECT members.firstname AS "Имя", members.lastname AS "Фамилия", books.title AS "Книга", borrowings.borrowdate AS "Когда взял", borrowings.returndate AS "Когда отдать" FROM borrowings JOIN books ON borrowings.bookid=books.bookid JOIN members ON borrowings.memberid=members.memberid WHERE firstname LIKE :fn;');
+       SQLQuery1.params.parambyname('fn').asstring := edit1.Text + '%';
+     end;
+    if ((sender as TEdit).Name = 'Edit2') then
+     begin
+       SQLQuery1.SQL.Add('SELECT members.firstname AS "Имя", members.lastname AS "Фамилия", books.title AS "Книга", borrowings.borrowdate AS "Когда взял", borrowings.returndate AS "Когда отдать" FROM borrowings JOIN books ON borrowings.bookid=books.bookid JOIN members ON borrowings.memberid=members.memberid WHERE lastname LIKE :ln;');
+       SQLQuery1.params.parambyname('ln').asstring := edit2.Text+'%';
+     end;
+  SQLQuery1.Open;
+end;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-
+MySQL57Connection1.Connected:=true;
+if (not MySQL57Connection1.Connected) then button1.Visible:=true;
 end;
 
 
